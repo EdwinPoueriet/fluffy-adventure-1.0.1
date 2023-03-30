f83e377c70628fecb5f05485f8eca5e3ac1f4793
Edwin <edwinpoueriet.dev@gmail.com>
2023-03-30 01:13:21 -0400

    add 5 7 5 ruler

--- solution.js
+++ solution.js
@@ -2,28 +2,44 @@ const fs = require('fs');
 
 const Haiku = () =>{
     const arr = fs.readFileSync("words.txt", "utf8").split("\n");
-    const result = [];
 
-    // console.log(arr)
+    const result = ["","",""];
+
+    result.forEach((_, index)=> {
+        let amount = index === 1 ? 7 : 5;
+        const words = [];
+        while (amount > 0){
+            
+            const currentWord = arr[Math.floor(Math.random() * arr.length)];
+            const currentWordSyllables = syllablesCounter(currentWord);
+            if(currentWordSyllables <= amount){
+                words.push(currentWord);
+                amount = amount - currentWordSyllables;
+            }
+        }
+        result[index] = words.join(" ");
+    })
+
+    console.log(result)
 }
 
 Haiku()
 
 function syllablesCounter(word) {
     let Word = word;
-    if (word.length == 0){
+    if (Word.length == 0){
         return 0;
     }
     if (Word.length <= 3) {
         return 1
     }
-    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-    word = word.replace(/^y/, '');
-    return word.match(/[aeiouy]{1,2}/g).length;
+    Word = Word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+    Word = Word.replace(/^y/, '');
+    return Word.match(/[aeiouy]{1,2}/g).length;
 }
 
 console.log(syllablesCounter("organized"))
 
-// check amount of syllables
+
 // create haiku  5 7 5 
 // avoid repeating words
