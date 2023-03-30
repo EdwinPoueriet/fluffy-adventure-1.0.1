46ec9337f06880e364a4a01e01a199e2ccbb81a2
Edwin <edwinpoueriet.dev@gmail.com>
2023-03-30 03:47:23 -0400

    prettier

--- solution.js
+++ solution.js
@@ -1,71 +1,67 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const Haiku = (arr) =>{
-    
-    if(!isArrOfStrings(arr)){
-        return [];
-    };
-
-    const result = ["","",""];
-    let copy = new Set();
-
-    result.forEach((_, index)=> {
-        let amount = index === 1 ? 7 : 5;
-        const words = [];
-        while (amount > 0){
-            
-            let currentWord = removeNonLetterCharacters(arr[Math.floor(Math.random() * arr.length)]);
-            
-            const currentWordSyllables = syllablesCounter(currentWord);
-
-            if(currentWordSyllables <= amount && !copy.has(currentWord)){
-                copy.add(currentWord);
-                words.push(currentWord);
-                amount = amount - currentWordSyllables;
-            }
-        }
-
-        result[index] = words.join(" ");
-    })
-
-    return result;
-}
-
-const file = fs.readFileSync("words.txt", "utf8").split("\n");
-console.log(Haiku(file))
-
-
-function syllablesCounter(word) {
-
-    let Word = word;
-
-    if (Word.length == 0){
-        return 0;
-    }
-
-    if (Word.length <= 3) {
-        return 1
-    }
-
-    Word = Word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-    Word = Word.replace(/^y/, '');
-
-    return Word.match(/[aeiouy]{1,2}/g).length;
-}
-
-
-function isArrOfStrings(arr) {
-    if (!Array.isArray(arr)) {
-        return false;
-      }
-    
-    return arr.every((word) => {
-        return typeof word === 'string';
-    });
-}
-function removeNonLetterCharacters (word) {
-    return word.replace(/[^a-zA-Z ]/g, '');
+const Haiku = (arr) => {
+  if (!isArrOfStrings(arr)) {
+    return [];
   }
 
-module.exports = {Haiku, syllablesCounter}
+  const result = ["", "", ""];
+  let copy = new Set();
 
+  result.forEach((_, index) => {
+    let amount = index === 1 ? 7 : 5;
+    const words = [];
+    while (amount > 0) {
+      let currentWord = removeNonLetterCharacters(
+        arr[Math.floor(Math.random() * arr.length)]
+      );
+
+      const currentWordSyllables = syllablesCounter(currentWord);
+
+      if (currentWordSyllables <= amount && !copy.has(currentWord)) {
+        copy.add(currentWord);
+        words.push(currentWord);
+        amount = amount - currentWordSyllables;
+      }
+    }
+
+    result[index] = words.join(" ");
+  });
+
+  return result;
+};
+
+const file = fs.readFileSync("words.txt", "utf8").split("\n");
+console.log(Haiku(file));
+
+function syllablesCounter(word) {
+  let Word = word;
+
+  if (Word.length == 0) {
+    return 0;
+  }
+
+  if (Word.length <= 3) {
+    return 1;
+  }
+
+  Word = Word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, "");
+  Word = Word.replace(/^y/, "");
+
+  return Word.match(/[aeiouy]{1,2}/g).length;
+}
+
+function isArrOfStrings(arr) {
+  if (!Array.isArray(arr)) {
+    return false;
+  }
+
+  return arr.every((word) => {
+    return typeof word === "string";
+  });
+}
+function removeNonLetterCharacters(word) {
+  return word.replace(/[^a-zA-Z ]/g, "");
+}
+
+module.exports = { Haiku, syllablesCounter };
--- solution.test.js
+++ solution.test.js
@@ -1,65 +1,63 @@
-const { describe } = require('node:test');
-const fs = require('fs');
+const { describe } = require("node:test");
+const fs = require("fs");
 
 const file = fs.readFileSync("words.txt", "utf8").split("\n");
 
-const {Haiku, syllablesCounter} = require('./solution')
+const { Haiku, syllablesCounter } = require("./solution");
 
-describe('Syllables counter', ()=>{
-    
-    test('should correctly count syllables in a word', () => {
-        expect(syllablesCounter('swole')).toBe(2);
-        expect(syllablesCounter('fit')).toBe(1);
-        expect(syllablesCounter('fitness')).toBe(2);
-        expect(syllablesCounter('gains')).toBe(1);
-    });
+describe("Syllables counter", () => {
+  test("should correctly count syllables in a word", () => {
+    expect(syllablesCounter("swole")).toBe(2);
+    expect(syllablesCounter("fit")).toBe(1);
+    expect(syllablesCounter("fitness")).toBe(2);
+    expect(syllablesCounter("gains")).toBe(1);
+  });
 
-    test('should return 0 for an empty string', () => {
-        expect(syllablesCounter('')).toBe(0);
-    });
+  test("should return 0 for an empty string", () => {
+    expect(syllablesCounter("")).toBe(0);
+  });
 
-    test('should return 1 for a word with 3 or fewer characters', () => {
-        expect(syllablesCounter('cat')).toBe(1);
-    });
+  test("should return 1 for a word with 3 or fewer characters", () => {
+    expect(syllablesCounter("cat")).toBe(1);
+  });
 });
 
-describe('Haiku', () => {
+describe("Haiku", () => {
+  test("Follows the 5 7 5 rule for Haiku", () => {
+    const result = Haiku(file);
+    const syllablesTotal = (str) => {
+      const arr = str.split(" ");
+      return arr.reduce((acc, val) => {
+        return acc + syllablesCounter(val);
+      }, 0);
+    };
+    expect(syllablesTotal(result[0])).toBe(5);
+    expect(syllablesTotal(result[1])).toBe(7);
+    expect(syllablesTotal(result[2])).toBe(5);
+  });
 
-    test('Follows the 5 7 5 rule for Haiku', () => {
-        const result = Haiku(file);
-        const syllablesTotal = (str) =>{
-            const arr = str.split(" ");
-            return arr.reduce((acc, val) =>{
-                return acc + syllablesCounter(val);
-            },0)
-        }
-        expect(syllablesTotal(result[0])).toBe(5);
-        expect(syllablesTotal(result[1])).toBe(7);
-        expect(syllablesTotal(result[2])).toBe(5);
-    });
+  test("no word can be duplicated", () => {
+    const result = Haiku(file);
+    const separatedWords = result.join(" ").split(" ");
+    expect(new Set(separatedWords).size).toBe(separatedWords.length);
+  });
 
-    test('no word can be duplicated', () => {
-        const result = Haiku(file);
-        const separatedWords = result.join(' ').split(' ');
-        expect(new Set(separatedWords).size).toBe(separatedWords.length);
-    });
+  test("returns only words with letters, not numbers.", () => {
+    const listWNumbers = addNumberToWords(file);
+    const result = Haiku(listWNumbers);
+    const words = result.join(" ").split(" ");
+    const containsNumbers = words.some((word) => /\d/.test(word));
+    expect(containsNumbers).toBe(false);
+  });
 
-    test('returns only words with letters, not numbers.', () => {
-        const listWNumbers = addNumberToWords(file)
-        const result = Haiku(listWNumbers);
-        const words = result.join(' ').split(' ');
-        const containsNumbers = words.some(word => /\d/.test(word));
-        expect(containsNumbers).toBe(false);
-    });
-    
-    // this is just in case a file comes with a page number we ignore it.
-    test('returns empty array when passed invalid input', () => {
-        const result = Haiku('not an array');
-        expect(Array.isArray(result)).toBe(true);
-        expect(result.length).toBe(0);
-    });
+  // this is just in case a file comes with a page number we ignore it.
+  test("returns empty array when passed invalid input", () => {
+    const result = Haiku("not an array");
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toBe(0);
+  });
 });
 
 function addNumberToWords(arr) {
-    return arr.map((word, index) => `${word}${index + 1}`);
+  return arr.map((word, index) => `${word}${index + 1}`);
 }
