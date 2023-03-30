baa84e0d93cbea1990284dd14c649c6106792d42
Edwin <edwinpoueriet.dev@gmail.com>
2023-03-30 04:19:50 -0400

    expanded tests and prettier

--- solution.test.js
+++ solution.test.js
@@ -7,17 +7,20 @@ const { Haiku, syllablesCounter } = require("./solution");
 
 describe("Syllables counter", () => {
   test("should correctly count syllables in a word", () => {
-    expect(syllablesCounter("swole")).toBe(2);
+    expect(syllablesCounter("smile")).toBe(2);
     expect(syllablesCounter("fit")).toBe(1);
-    expect(syllablesCounter("fitness")).toBe(2);
-    expect(syllablesCounter("gains")).toBe(1);
+    expect(syllablesCounter("exercising")).toBe(4);
   });
 
-  test("should return 0 for an empty string", () => {
+  test("returns 0 for an empty string or not a string at all", () => {
     expect(syllablesCounter("")).toBe(0);
+    expect(syllablesCounter(5)).toBe(0);
+    expect(syllablesCounter(null)).toBe(0);
+    expect(syllablesCounter([])).toBe(0);
+    expect(syllablesCounter(undefined)).toBe(0);
   });
 
-  test("should return 1 for a word with 3 or fewer characters", () => {
+  test("returns 1 for a word with 3 or fewer characters", () => {
     expect(syllablesCounter("cat")).toBe(1);
   });
 });
@@ -42,7 +45,8 @@ describe("Haiku", () => {
     expect(new Set(separatedWords).size).toBe(separatedWords.length);
   });
 
-  test("returns only words with letters, not numbers.", () => {
+  // this is just in case a file comes with a page number we ignore it.
+  test("returns only letters, not numbers or characters.", () => {
     const listWNumbers = addNumberToWords(file);
     const result = Haiku(listWNumbers);
     const words = result.join(" ").split(" ");
@@ -50,7 +54,6 @@ describe("Haiku", () => {
     expect(containsNumbers).toBe(false);
   });
 
-  // this is just in case a file comes with a page number we ignore it.
   test("returns empty array when passed invalid input", () => {
     const result = Haiku("not an array");
     expect(Array.isArray(result)).toBe(true);
